9c35f6be290fcbbc2891c51f7877582ee1eb2788
JFuton847 <jimmyfuton847@gmail.com>
2025-02-27 14:00:34 -0600

    add imports to Main.jsx

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NewsCard from "../../components/NewsCard/NewsCard";
+import About from "../../components/About/About";
+
+<main>
+  <NewsCard />
+  <About />
+</main>;
